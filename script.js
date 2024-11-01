d21607155cee523809d55621be1f66a9efe6234c
FabianRebert <fabian.glitz01@gmail.com>
2024-11-01 15:31:42 +0530

    changes

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const video = document.getElementById('video');
 
 async function startVideoStream() {
     try {
-        // Get access to the camera
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: { facingMode: { exact: 'environment' } } // Accesses the back camera
+        });
         video.srcObject = stream;
-
-        // Here you can add the WebRTC connection setup to send the stream
-        // For example, using RTCPeerConnection to connect to a remote peer
     } catch (error) {
         console.error('Error accessing media devices.', error);
     }
 }
+
 
 startVideoStream();
