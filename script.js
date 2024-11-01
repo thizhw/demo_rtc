833578d9c59be5d6dd21ea1fee4714a346480d83
FabianRebert <fabian.glitz01@gmail.com>
2024-11-01 15:36:55 +0530

    changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const video = document.getElementById('video');
 async function startVideoStream() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({
-            video: { facingMode: { exact: 'environment' } } // Accesses the back camera
+            video: { facingMode: method == 2 ? "user" : "environment", } // Accesses the back camera
         });
         video.srcObject = stream;
     } catch (error) {
